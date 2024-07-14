910d80cdae7cb3de4c739b255e141da21aed6ea1
Xamarindev2023 <xamarindeveloper2021@gmail.com>
2024-07-14 13:59:39 +0530

    Complete Implementation .NET MAUI Firebase

---
+++ FirebaseSample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace FirebaseSample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private string _deviceToken;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            if (Preferences.ContainsKey("DeviceToken"))
+            {
+                _deviceToken = Preferences.Get("DeviceToken", "");
+            }
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ FirebaseSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FirebaseSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
